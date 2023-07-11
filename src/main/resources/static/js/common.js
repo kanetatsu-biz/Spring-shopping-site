cbc5380df45885811790c6286054908c808c8203
kakerutsukui <kakerutsukui@localhost>
2023-07-11 10:36:09 +0900

    修正：コード修正 #25
    ・インデントの修正
    ・末尾に;の追記
    ・varをconstに変更
    ・処理の説明を追記
    ・console.logを消去

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,29 +1,30 @@
 'use strict';
 
-var head = document.getElementsByTagName('head')
-var script = document.createElement('script')
-//ここで任意のjQueryを読み込ませる
-script.setAttribute('src', 'https://code.jquery.com/jquery-3.7.0.js')
-script.setAttribute('type', 'text/javascript')
-document.head.appendChild(script)
+const head = document.getElementsByTagName('head');
+const script = document.createElement('script');
+//任意のjQueryを読み込ませる
+script.setAttribute('src', 'https://code.jquery.com/jquery-3.7.0.js');
+script.setAttribute('type', 'text/javascript');
+document.head.appendChild(script);
 
+//渡ってきた商品IDを元に数量を変更処理→戻ってきた値を元にhtmlを書き換え
+function changeQuantity(selected_id, quantity) {
+	const url = '/cart/update/' +  selected_id;
+	const target = document.getElementById("cart-items");
 
-	function changeQuantity(selected_id, quantity) {
-		console.log(selected_id);
-		console.log(quantity);
-    	const url = '/cart/update/' +  selected_id;
-    	const target = document.getElementById("cart-items");
-
+	//変更された数量をpost形式で渡す
     $.ajax({
-      method: 'POST',
-      data: {quantity},
-      url: url
-    }).then(
-            function(data) {
-              target.outerHTML = data;
-            },
-            function() {
-              alert("error");
-            });
-  }
+		method: 'POST',
+		data: {quantity},
+		url: url
+	}).then(
+		//戻り値でhtmlを書き換え
+		function(data) {
+			target.outerHTML = data;
+		},
+		function() {
+			alert("error");
+		}
+	);
+}
  
