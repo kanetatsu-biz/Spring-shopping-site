9104fc027b3095bea003d906a5c61899d6bd0c76
kakerutsukui <kakerutsukui@192.168.1.11>
2023-08-01 16:18:46 +0900

    修正：詳細ページもすでにカートに入っている商品の在庫を変更 #32

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -70,6 +70,16 @@ public class ItemController {
 
 		//	商品IDをもとに商品を取得
 		Item item = itemRepository.findById(itemId).get();
+		//すでにカートに入っている商品は在庫を変更
+		for (Item cartItem : cart.getItems()) {
+			//	カートに既に商品が存在している場合
+			if (item.getId() == cartItem.getId()) {
+				//	在庫情報をもとに購入可能な数量を計算しなおす
+				item.setStock(item.getStock() - cartItem.getQuantity());
+				break;
+			}
+		}
+
 		model.addAttribute("item", item);
 
 		return "showItem";
