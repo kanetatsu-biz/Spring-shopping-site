470390d17bf4a1af95c92d3713bd400efa6f495c
kakerutsukui <kakerutsukui@localhost>
2023-08-23 15:45:15 +0900

    修正：型の修正 #10

--- src/main/java/com/example/demo/repository/VLoginUserOrderHistoryRepository.java
+++ src/main/java/com/example/demo/repository/VLoginUserOrderHistoryRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +8,5 @@ import com.example.demo.entity.VLoginUserOrderHistory;
 
 public interface VLoginUserOrderHistoryRepository extends JpaRepository<VLoginUserOrderHistory, Integer> {
 	// SELECT * FROM v_loginUser_order_histories WHERE id = ?
-	Optional<List<VLoginUserOrderHistory>> findByCustomerId(Integer customerId);
+	List<VLoginUserOrderHistory> findByCustomerId(Integer customerId);
 }
