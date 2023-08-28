7ee809f0ac2eb277b8c0b611b63f9eff0d395df2
kakerutsukui <kakerutsukui@localhost>
2023-08-28 16:40:41 +0900

    修正：ほしい物リストのフィールド修正 #12

---
+++ src/main/java/com/example/demo/entity/WishList.java
@@ -0,0 +1,72 @@
+package com.example.demo.entity;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Component
+@Entity
+@Table(name = "wish_lists")
+public class WishList {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "customer_id")
+	private Integer customerId;
+
+	@Column(name = "item_id")
+	private Integer itemId;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "file_name")
+	private String fileName; // 画像ファイル名
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+	public Integer getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+}
