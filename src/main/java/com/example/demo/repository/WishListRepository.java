8e3f56ea174f1d7e18330e6326d7d5743425a142
kakerutsukui <kakerutsukui@192.168.1.11>
2023-09-10 11:07:32 +0900

    修正：処理の追加 #12
    ・重複しているほしい物をチェック
    ・指定されたほしい物をテーブルから削除

--- src/main/java/com/example/demo/repository/WishListRepository.java
+++ src/main/java/com/example/demo/repository/WishListRepository.java
@@ -5,8 +5,21 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.WishList;
+import com.example.demo.entity.WishListKey;
 
-public interface WishListRepository extends JpaRepository<WishList, Integer> {
+import jakarta.transaction.Transactional;
 
+public interface WishListRepository extends JpaRepository<WishList, WishListKey> {
+
+	//ほしい物テーブルから対象のユーザーのほしい物を取得
 	List<WishList> findByCustomerId(Integer customerId);
+
+	//すでにほしい物リストに追加されているかチェック
+	boolean existsByCustomerIdAndItemId(Integer customerId, Integer itemId);
+
+	//ほしい物テーブルから指定されたほしい物を削除
+	// DELETE FROM wishes WHERE customer = ? AND item_id = ?
+	@Transactional
+	void deleteByCustomerIdAndItemId(Integer customerId, Integer itemId);
+
 }
